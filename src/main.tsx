2df6f748d3846ca458b2d50694b8adc47ad21dfb
lucasspecht1 <lucasspecht4@gmail.com>
2024-06-12 13:09:54 -0300

    Arquivos iniciais Projeto

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { SafeAreaView, View, Text, Pressable, Alert } from "react-native"
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <View>
+            </View>
+        </SafeAreaView>
+    );
+}

d49a41d5a2b0e96938bbcfd83f4242603ed11a3e
lucasspecht1 <lucasspecht4@gmail.com>
2024-06-12 17:03:16 -0300

    Esboço componente tela de Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { SafeAreaView, View, Text, Pressable, Alert } from "react-native"
+import { SafeAreaView } from "react-native"
+import Login from "./login/login"
 
 export default function App() {
     return (
-        <SafeAreaView>
-            <View>
-            </View>
+        <SafeAreaView style={{backgroundColor: '#1F1F50'}}>
+            <Login/>
         </SafeAreaView>
     );
 }
